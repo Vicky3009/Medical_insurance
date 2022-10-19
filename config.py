b1fd8ddd64f4990ad1a950fddea228a427f08246
unknown <vikassukasepatil@gmail.com>
2022-10-19 11:03:49 +0530

    Configuration File

---
+++ config.py
@@ -0,0 +1,11 @@
+
+from fastjsonschema import JsonSchemaDefinitionException
+
+
+import os
+
+PORT_NUMBER = 5507
+
+MODEL_FILE_PATH = os.path.join(os.getcwd(),r'\models\Linear_regression.pkl')
+
+JSON_FILE_PATH = os.path.join(os.getcwd(),r'\models\project_data.pkl')
